119c9c87bfd0ef1729490e29f57536156132eb5d
Owen Jones <owenthefirst93@gmail.com>
2022-02-06 12:23:37 -0500

    edit form on appt card

--- client/src/Components/AppointmentCard.jsx
+++ client/src/Components/AppointmentCard.jsx
@@ -2,12 +2,31 @@ import React,{useState} from 'react'
 
 const AppointmentCard = ({setAppointments, appointments, id, doctorName, patientName, phoneNumber, email, reasonForVisit,deleted,allowEdit,setAllowEdit}) => {
 
+    const [newDoctorName, setNewDoctorName] = useState(doctorName)
+    const [newPatientName, setNewPatientName] = useState(patientName)
+    const [newPhoneNumber, setNewPhoneNumber] = useState(phoneNumber)
+    const [newEmail, setNewEmail] = useState(email)
+    const [newReasonForVisit, setNewReasonForVisit] = useState(reasonForVisit)
+
     const editAppointment = () => {
         setAllowEdit(!allowEdit)
     }
-    
-    const handleEditSubmission = () => {
+
+    const handleEditSubmission = (e) => {
         console.log('trying')
+        e.preventDefault()
+        console.log(appointments)
+        let editedAppointment = {
+            id: id,
+            doctorName: newDoctorName,
+            email: '',
+            patientName:'',
+            phoneNumber:'',
+            reasonForVisit:'',
+        }
+        // setAllowEdit(!allowEdit)
+        // setAppointments([...appointments, editedAppointment])
+        // appointments.splice(1,editAppointment)
     }
 
     const deleteAppointmentCard = (id) => {
@@ -28,10 +47,48 @@ const AppointmentCard = ({setAppointments, appointments, id, doctorName, patient
 
             {allowEdit && 
             <form onSubmit={handleEditSubmission}>
-                <input />
-                <input />
-                <input />
-                <input />
+                <input 
+                    name={'edit-docname'}
+                    placeholder={'doctorName'}
+                    value={newDoctorName}
+                    onChange={(e) => setNewDoctorName(e.target.value)}
+                />
+                <input 
+                    type='text' 
+                    name='patient' 
+                    id='patientname' 
+                    placeholder='Patient Name'
+                    onChange = {(e) => setNewPatientName(e.target.value)} 
+                    value={newPatientName}
+                />
+                <input 
+                    type='text' 
+                    name='phone' 
+                    id='phone' 
+                    placeholder='Phone Number' 
+                    onChange = {(e) => setNewPhoneNumber(e.target.value)}
+                    value={newPhoneNumber}
+                />
+                <input 
+                    type='text' 
+                    name='email' 
+                    id='email' 
+                    placeholder='Email' 
+                    onChange = {(e) => setNewEmail(e.target.value)}
+                    value={newEmail}
+                />
+                <select 
+                    name='visit-reason'
+                    value={newReasonForVisit}
+                    onChange={(e) => setNewReasonForVisit(e.target.value)}
+                    >
+                    <option value='' >{'Reason for visit'}</option>
+                    <option>{'Sick'}</option>
+                    <option>{'Emergency'}</option>
+                    <option>{'Check-up'}</option>
+                    <option>{'Physical'}</option> 
+                    <option>{'Referral'}</option>
+                </select>
                 <button>Submit</button>
             </form>
             
